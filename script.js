87ede0c68e2b45ec61f50a55dc5171d0e3cb6c7b
Saigunas <saigunasb777@gmail.com>
2022-04-12 13:19:05 +0300

    Added player objects and turn change

--- script.js
+++ script.js
@@ -13,11 +13,34 @@ const Gameboard = (() => {
     }
     showGameBoard();
 
+    const Player = (name, sign) => {
+        return {name, sign};
+    };
+
+    let p1 = Player("Morgana", 'M');
+    let p2 = Player("Ike", "I");
+    
+    const TurnManager = ((p1, p2) => {
+        let whoseTurn = p2;
+
+        function changeTurn() {
+            if(whoseTurn === p1) 
+                whoseTurn = p2;
+            else
+                whoseTurn = p1;
+
+            return whoseTurn;
+        }
+
+        return {changeTurn}
+
+    })(p1, p2);
+
     gridBlocks.forEach(block => block.addEventListener('click', checkBlock));
     function checkBlock(e) {
         let index = e.target.getAttribute('data-index');
         if(gameboard[index] === '') {
-            gameboard[index] = 'X';
+            gameboard[index] = TurnManager.changeTurn().sign;
             showGameBoard();
         }
     }
